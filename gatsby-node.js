3e1532761c8393e4ba30105cb57943de422ed47c
DevFreddy <michael.frederickjr@gmail.com>
2020-04-15 12:15:36 -0400

    feat: add relationship between project and stats

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+
+const fs = require(`fs`)
+
+/**
+ * Custom Type Definitions
+ */
+exports.createSchemaCustomization = ({ actions, schema }) => {
+  const { createTypes } = actions
+
+  const additionalTypeDefs = fs.readFileSync(`type-defs.gql`, {
+    encoding: `utf-8`,
+  })
+  createTypes(additionalTypeDefs)
+}
