3505eb62924289cdc1be2aba6d560c8a80ebd640
DevFreddy <michael.frederickjr@gmail.com>
2020-04-22 11:20:39 -0400

    fix: revert change to placeholder readme

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -133,8 +133,59 @@ const ProjectPage = ({ data }) => {
       />
       <div className="primary-content">
         <main className="primary-content-main">
-          {hasReadme && <ProjectReadme mdx={project.mainContent.content} />}
-          {!hasReadme && <h2>No content found.</h2>}
+          <h2>Integer posuere erat a ante venenatis</h2>
+          <p>
+            Donec sed odio dui. Donec sed odio dui. Cras justo odio, dapibus ac
+            facilisis in, egestas eget quam. Duis mollis, est non commodo
+            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+            Vestibulum id ligula porta felis euismod semper. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </p>
+
+          <h3>Integer posuere erat a ante venenatis</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit.
+          </p>
+
+          <h3>What people are saying</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit.
+          </p>
+
+          <blockquote>
+            Vitae enim egestas egestas at gravida arcu, amet in. Facilisis at
+            massa amet, aliquet dui semper. Sit placerat sed et ornare faucibus
+            egestas sit nisl, diam.
+            <cite>Leslie Webb</cite>
+          </blockquote>
+
+          <blockquote>
+            Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+            vestibulum. Maecenas faucibus mollis interdum. Maecenas sed diam
+            eget risus varius blandit sit amet non magna.
+            <cite>Bildad the Shuhite</cite>
+          </blockquote>
+
+          <h3>Mattis risus ultricies</h3>
+          <p>
+            Curabitur blandit tempus porttitor. Cum sociis natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus. Nullam id
+            dolor id nibh ultricies vehicula ut id elit. Donec id elit non mi
+            porta gravida at eget metus.
+          </p>
+          <p>
+            Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo
+            odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non
+            mi porta gravida at eget metus. Donec ullamcorper nulla non metus
+            auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+            auctor.
+          </p>
 
           <h3>Top Contributors</h3>
           <p>Thanks goes to these wonderful people:</p>
