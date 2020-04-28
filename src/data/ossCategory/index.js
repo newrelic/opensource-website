853467376b4d650357253bca437f8188f4ac58dc
Joel Worrall <jworrall@newrelic.com>
2020-04-28 11:56:14 -0400

    feat: adding ossCategory. Closes #318.

---
+++ src/data/ossCategory/index.js
@@ -0,0 +1,26 @@
+[
+  {
+    "title": "New Relic One Catalog Project",
+    "slug": "new-relic-one-catalog-project"
+  },
+  {
+    "title": "Community Project",
+    "slug": "community-project"
+  },
+  {
+    "title": "Example Code",
+    "slug": "example-code"
+  },
+  {
+    "title": "Product delivered in Open Source",
+    "slug": "product-delivered-in-open-source"
+  },
+  {
+    "title": "New Relic Experimental",
+    "slug": "new-relic-experimental"
+  },
+  {
+    "title": "Archived",
+    "slug": "archived"
+  }
+]
