newrelic/opensource-website

commit 18bc4573d15b0cc0d7cfb0420845a77d3dfbe5c3

feat(Issue #3): hide featured projects when searching and order by most commits desc

src/components/ProjectSearch.js
@@ -213,7 +213,7 @@ class ProjectSearch extends Component {
               onFilterChange={this.onFilterChange}
             />
           </form>
-          <div>{children({ projects: filterResults })}</div>
+          <div>{children({ projects: filterResults, searchQuery })}</div>
         </div>
       </div>
     );

src/templates/explore-projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Link } from 'gatsby';
+import { orderBy } from 'lodash';
+
 import Layout from '../components/layout';
 import PageHeading from '../components/PageHeading';
 import ProjectSearch from '../components/ProjectSearch';
@@ -28,8 +30,11 @@ const ExploreProjectsPage = props => {
     allProjectTypes: { title: 'Type', options: allProjectTypes }
   };
 
-  const renderFeaturedProjects = projects => {
-    return projects.slice(0, 3).map(p => {
+  const featuredProjectsToShow = 3;
+  const projectsToShow = 24; // Set to 0 for all
+
+  const renderFeaturedProjects = ({ projects, featuredProjectsToShow }) => {
+    return projects.slice(0, featuredProjectsToShow).map(p => {
       const link = p.permalink.replace('https://opensource.newrelic.com', '');
 
       return (
@@ -70,9 +75,10 @@ const ExploreProjectsPage = props => {
   };
 
   const renderProjectListing = projects => {
-    const projectsToShow = 20; // Set to 0 for all
     const projectsToList =
-      projectsToShow > 0 ? projects.slice(0, projectsToShow) : projects;
+      projectsToShow > 0
+        ? projects.slice(featuredProjectsToShow, projectsToShow)
+        : projects;
 
     return projectsToList.map(p => {
       return <ProjectCard key={p.id} project={p} />;
@@ -90,15 +96,27 @@ const ExploreProjectsPage = props => {
         engine={searchEngineOptions}
         filterOptions={filterOptions}
       >
-        {({ projects }) => {
+        {({ projects, searchQuery }) => {
+          const sortedProjects = orderBy(
+            projects,
+            p => {
+              return p.stats.commits;
+            },
+            'desc'
+          );
+
           return (
             <>
               <div className={styles.featuredProjects}>
-                {renderFeaturedProjects(projects)}
+                {searchQuery === '' &&
+                  renderFeaturedProjects({
+                    projects: sortedProjects,
+                    featuredProjectsToShow
+                  })}
               </div>
 
               <div className={styles.projectListingContainer}>
-                {renderProjectListing(projects)}
+                {renderProjectListing(sortedProjects)}
               </div>
             </>
           );

commit c53ab6faccb54a51120b0a82e2dcc2bf3480858b

fix: search regression
Caused primarily by changes in 18bc457 , also partially because I misused lodash's "isEmpty" function again :facepunch:

src/components/ProjectSearch.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as JsSearch from 'js-search';
-import { isEmpty } from 'lodash';
 
 import ProjectSearchInput from './ProjectSearchInput';
 
@@ -123,11 +122,22 @@ class ProjectSearch extends Component {
         this.filterData
       );
     }
+
+    if (searchQuery && !hasFilter) {
+      this.setState({
+        searchQuery,
+        searchResults: queryResult,
+        filterResults: queryResult
+      });
+    }
   };
 
   hasFilter = () => {
     const { filterValues } = this.state;
-    return !isEmpty(filterValues);
+    const isEmpty = !Object.values(filterValues).some(
+      x => x !== null && x !== ''
+    );
+    return !isEmpty;
   };
 
   onFilterChange = ({ field, value }) => {

src/templates/explore-projects.js
@@ -74,11 +74,13 @@ const ExploreProjectsPage = props => {
     });
   };
 
-  const renderProjectListing = projects => {
+  const renderProjectListing = ({ projects, showFeatured }) => {
+    const start = showFeatured ? featuredProjectsToShow : 0;
+    const end =
+      projects.length >= projectsToShow ? projectsToShow : projects.length;
+
     const projectsToList =
-      projectsToShow > 0
-        ? projects.slice(featuredProjectsToShow, projectsToShow)
-        : projects;
+      projectsToShow > 0 ? projects.slice(start, end) : projects;
 
     return projectsToList.map(p => {
       return <ProjectCard key={p.id} project={p} />;
@@ -97,6 +99,7 @@ const ExploreProjectsPage = props => {
         filterOptions={filterOptions}
       >
         {({ projects, searchQuery }) => {
+          const showFeatured = searchQuery === '';
           const sortedProjects = orderBy(
             projects,
             p => {
@@ -108,7 +111,7 @@ const ExploreProjectsPage = props => {
           return (
             <>
               <div className={styles.featuredProjects}>
-                {searchQuery === '' &&
+                {showFeatured &&
                   renderFeaturedProjects({
                     projects: sortedProjects,
                     featuredProjectsToShow
@@ -116,7 +119,10 @@ const ExploreProjectsPage = props => {
               </div>
 
               <div className={styles.projectListingContainer}>
-                {renderProjectListing(sortedProjects)}
+                {renderProjectListing({
+                  projects: sortedProjects,
+                  showFeatured
+                })}
               </div>
             </>
           );