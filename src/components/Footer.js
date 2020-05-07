722d9d0bac069b6f4dcb64e04687eb45f144cf1b
DevFreddy <michael.frederickjr@gmail.com>
2020-05-07 13:37:09 -0400

    feat: add edit link driven by different rules for different pages, Issue #342

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
-import { Link } from 'gatsby';
 import React from 'react';
-import navLinks from '../data/navigation.json';
+import PropTypes from 'prop-types';
 
+import { Link } from 'gatsby';
+import { Location, Match } from '@reach/router';
+import { getEditLinkFromLocation } from '../utils';
+
+import navLinks from '../data/navigation.json';
 import styles from './Footer.module.scss';
 import editIcon from '../images/icon-edit.svg';
 
@@ -25,33 +29,63 @@ const Footer = () => {
   };
 
   return (
-    <footer className={styles.footerContainer}>
-      <div className={styles.footerContents}>
-        <div className={styles.footerLogoContainer}>
-          <Link to="/" className={styles.footerLogo}>
-            New Relic Open Source
-          </Link>
-          <a href="#" className={styles.editButton}>
-            <img
-              src={editIcon}
-              alt="pencil icon"
-              className={styles.editButtonIcon}
-            />
-            Edit this page
-          </a>
-        </div>
-        <div className={styles.meta}>
-          <small className={styles.version}>
-            Version {packageJson.version}
-          </small>
-          <small className={styles.footerLegal}>
-            Copyright &copy; {new Date().getFullYear()} New Relic Inc.
-          </small>
-        </div>
-        <ul className={styles.footerNavLinks}>{renderNavLinks()}</ul>
-      </div>
-    </footer>
+    <Location>
+      {({ location }) => {
+        const editLink = getEditLinkFromLocation({ location });
+
+        return (
+          <Match path={location.pathname}>
+            {// eslint-disable-next-line no-unused-vars
+            ({ location, match }) => {
+              // TO DO - Remove <Match>? It seemingly adds no additional context outside of what <Location> gives us
+              return (
+                <footer className={styles.footerContainer}>
+                  <div className={styles.footerContents}>
+                    <div className={styles.footerLogoContainer}>
+                      <Link to="/" className={styles.footerLogo}>
+                        New Relic Open Source
+                      </Link>
+                      {editLink && (
+                        <a
+                          href={editLink}
+                          rel="noopener noreferrer"
+                          className={styles.editButton}
+                          target="__blank"
+                        >
+                          <img
+                            src={editIcon}
+                            alt="pencil icon"
+                            className={styles.editButtonIcon}
+                          />
+                          Edit this page
+                        </a>
+                      )}
+                    </div>
+                    <div className={styles.meta}>
+                      <small className={styles.version}>
+                        Version {packageJson.version}
+                      </small>
+                      <small className={styles.footerLegal}>
+                        Copyright &copy; {new Date().getFullYear()} New Relic
+                        Inc.
+                      </small>
+                    </div>
+                    <ul className={styles.footerNavLinks}>
+                      {renderNavLinks()}
+                    </ul>
+                  </div>
+                </footer>
+              );
+            }}
+          </Match>
+        );
+      }}
+    </Location>
   );
+};
+
+Footer.propTypes = {
+  //
 };
 
 export default Footer;
