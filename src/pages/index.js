5c08432269d3203062c0048ee1758567b5d2d1a1
Daniel Golden <dgolden@newrelic.com>
2020-05-08 11:50:28 -0400

    chore: tweak homepage hero video animation
    - oh and actually make the video visible
    - Adds react-helmet so that I can add a class
    to the body element.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { graphql, Link } from 'gatsby';
 import { get } from 'lodash';
+import Helmet from 'react-helmet';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -19,6 +20,7 @@ import articlePlaceholderImage2 from '../images/article-placeholder-image-2.jpg'
 import articlePlaceholderImage3 from '../images/article-placeholder-image-3.jpg';
 import videoPlaceholder from '../images/video-placeholder.jpg';
 import playButton from '../images/button-play.svg';
+import closeIcon from '../images/icon-close.svg';
 
 export const query = graphql`
   query HomePageQuery {
@@ -36,7 +38,7 @@ export const query = graphql`
 `;
 
 const IndexPage = ({ data }) => {
-  const [playButtonHovered, setPlayButtonHovered] = useState(false);
+  const [heroVideoActive, setHeroVideoActive] = useState(false);
   const externalProjects = [
     {
       title: 'Open Telemetry',
@@ -94,6 +96,10 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout fullWidth>
+      <Helmet>
+        <body className={heroVideoActive && styles.heroVideoActive} />
+      </Helmet>
+
       <SEO title="Home" />
       <div className={styles.heroContainer}>
         <div className={styles.homepageHeroCopy}>
@@ -112,31 +118,42 @@ const IndexPage = ({ data }) => {
           </p>
         </div>
         <div
-          className={`${styles.homepageHeroVideo} ${playButtonHovered &&
-            styles.playButtonHovered}`}
+          className={`${styles.homepageHeroVideo}`}
           style={{ backgroundImage: `url(${videoPlaceholder})` }}
+          onClick={() => {
+            setHeroVideoActive(true);
+          }}
         >
           <img
-            src={playButton}
-            className={styles.playButton}
-            onMouseOver={() => {
-              setPlayButtonHovered(true);
-            }}
-            onMouseOut={() => {
-              setPlayButtonHovered(false);
+            src={closeIcon}
+            alt="close icon"
+            className={styles.modalCloseButton}
+            onClick={() => {
+              setHeroVideoActive(false);
             }}
           />
-          <iframe
-            style={{ display: 'none' }}
-            width="426"
-            height="240"
-            src="https://www.youtube.com/embed/7wnav6Fu9T0"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
+
+          <img src={playButton} className={styles.playButton} />
+          <div className={styles.iframeContainer}>
+            <iframe
+              className={styles.heroVideoIframe}
+              width="1000"
+              height="562.704471"
+              src={`https://www.youtube-nocookie.com/embed/7wnav6Fu9T0?showinfo=0&modestbranding=1&rel=0&controls=0${
+                heroVideoActive ? `&autoplay=1` : ''
+              }`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; showinfo; modestbranding"
+            />
+          </div>
         </div>
       </div>
+      <div
+        className={styles.videoModalOverlay}
+        onClick={() => {
+          setHeroVideoActive(false);
+        }}
+      />
 
       <HomePageHighlights data={externalProjects} />
 
