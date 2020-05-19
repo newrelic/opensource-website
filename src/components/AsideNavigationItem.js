9f4110946d6e9a2ece5bcb85bc0e55b6ab455afb
Daniel Golden <dgolden@newrelic.com>
2020-05-19 11:44:20 -0400

    feat(asideNav): make it responsive
    #370

--- src/components/AsideNavigationItem.js
+++ src/components/AsideNavigationItem.js
@@ -6,10 +6,18 @@ import chevronIcon from '../images/icon-chevron-right.svg';
 
 const AsideNavigationItem = ({ icon, label, subLabel, to }) => {
   return (
-    <a href={to} target="__blank" className={styles.itemContainer}>
+    <a
+      href={to}
+      target="__blank"
+      className={`${styles.itemContainer} ${
+        subLabel ? styles.hasSublabel : ''
+      }`}
+    >
       <img src={icon} className={styles.itemIcon} alt="icon for Project Name" />
-      <span className={styles.itemName}>{label}</span>
-      {subLabel && <span className={styles.itemDescription}>{subLabel}</span>}
+      <div className={styles.itemCopy}>
+        <span className={styles.itemName}>{label}</span>
+        {subLabel && <span className={styles.itemSublabel}>{subLabel}</span>}
+      </div>
       <img
         className={styles.itemChevron}
         src={chevronIcon}
