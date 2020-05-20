c5bda4e7ba7a6f8ad4a4a373789ada05a01b4dcb
Daniel Golden <dgolden@newrelic.com>
2020-05-20 08:31:40 -0400

    chore: add a broken image utility to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,12 @@
+import placeholderProjectIcon from '../images/page-heading-icon-placeholder.jpg';
+
 /*
  * Generic utilities
  */
 export const isEmptyObject = obj => {
   return !Object.values(obj).some(x => x !== null && x !== '');
+};
+
+export const handleBrokenImages = (e, replacementImage) => {
+  e.target.src = replacementImage || placeholderProjectIcon;
 };
