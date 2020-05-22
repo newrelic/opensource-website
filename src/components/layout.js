3bf3ddabab2afcf725b1e47c92a79ed5eb1f8a28
DevFreddy <michael.frederickjr@gmail.com>
2020-05-22 15:52:04 -0400

    refactor: header component rename

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
-import Header from './header';
+import Header from './Header';
 import Footer from './Footer';
 import './styles.scss';
 import favicon from '../images/favicon.svg';
