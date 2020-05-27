69e7442f679f6524ab67fac6eec5d0218d317324
Joel Worrall <jworrall@newrelic.com>
2020-05-27 14:11:38 -0400

    feat: add google tag manager. See #376

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-TK2F7T",
+        includeInDevelopment: true
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
