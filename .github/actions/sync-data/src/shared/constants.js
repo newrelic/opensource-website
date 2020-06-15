fc75381f1a6ee49e625317ec178e31040c3d5099
Justin Eveland <jeveland@newrelic.com>
2020-06-15 14:33:56 -0400

    chore(data): updating excluded projects for sync-data action

--- .github/actions/sync-data/src/shared/constants.js
+++ .github/actions/sync-data/src/shared/constants.js
@@ -17,17 +17,29 @@ const ORG_REPOS = [
   // }
 ];
 
+// These entries should match the `fullName` in the corresponding project's json file
 const EXCLUDED_PROJECTS = [
   'adopt-open-jdk',
-  'open-telemetry/opentelemetry-go',
-  'open-telemetry/opentelemetry-java',
-  'open-telemetry',
-  'w3c-distributed-tracing-wg',
   'newrelic/developer-toolkit',
   'newrelic/extends_newrelic_rpm',
   'newrelic/open-source-office',
   'newrelic/open-source-tools',
-  'newrelic/sidecar'
+  'newrelic/sidecar',
+  'open-telemetry',
+  "open-telemetry-opentelemetry-auto-instr-java",
+  "open-telemetry-opentelemetry-collector",
+  "open-telemetry-opentelemetry-cpp",
+  "open-telemetry-opentelemetry-dotnet",
+  "open-telemetry-opentelemetry-erlang",
+  'open-telemetry/opentelemetry-go',
+  'open-telemetry/opentelemetry-java',
+  "open-telemetry-opentelemetry-ruby",
+  "open-telemetry/opentelemetry-specification",
+  "w3c-correlation-context",
+  "w3c-distributed-tracing-wg",
+  "w3c-trace-context",
+  "w3c-trace-context-binary",
+  "w3c-trace-response"
 ];
 
 // TO DO - Ascertain Github's GraphQL query limits
