26b6fee635799529c92a053959b563d9956404ec
Joel Worrall <jworrall@newrelic.com>
2020-06-16 18:10:03 -0400

    fix(gatsby-browser): page identify and track

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+exports.onRouteUpdate = () => {
+  window.analytics && window.analytics.identify();
+  window.analytics && window.analytics.page();
+};
