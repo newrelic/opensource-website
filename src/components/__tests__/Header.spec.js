1aa078cc44f3377c64a3c6527ce1a0182fb22cd4
Jerel Miller <jmiller@newrelic.com>
2020-08-03 00:24:19 -0700

    test: fix broken Header test due to useStaticQuery usage

--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { useStaticQuery } from 'gatsby';
 
 import Header from '../Header';
+
+beforeEach(() => {
+  useStaticQuery.mockImplementation(() => ({
+    site: {
+      siteMetadata: {
+        title: 'New Relic Open Source',
+        description:
+          "New Relic <3's open source. We built this site to make it easy for you to explore the projects we're maintaining as well as our involvement in open standards.",
+        author: '@newrelic'
+      },
+      layout: {
+        maxWidth: '1180px'
+      }
+    }
+  }));
+});
 
 describe('Header', () => {
   it('renders correctly', () => {
