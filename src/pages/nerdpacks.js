71d3ecc1c190adb23c1f2b5eb8eca46f50362c48
Justin Eveland <jeveland@newrelic.com>
2020-09-01 17:25:19 -0400

    chore: update src/pages/nerdpacks.js link

--- src/pages/nerdpacks.js
+++ src/pages/nerdpacks.js
@@ -67,7 +67,7 @@ const NerdpackPage = ({ data }) => {
           <iframe
             width="578"
             height="328.125"
-            src="https://youtu.be/7omo0qHxku8"
+            src="https://www.youtube.com/embed/7omo0qHxku8"
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
