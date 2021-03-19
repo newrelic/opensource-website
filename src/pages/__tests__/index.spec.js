5d3b943fd420d845dcf03ddd90ccf5c39e853c87
Jerel Miller <jmiller@newrelic.com>
2021-03-18 17:52:32 -0700

    chore: fix tests for home page

--- src/pages/__tests__/index.spec.js
+++ src/pages/__tests__/index.spec.js
@@ -7,6 +7,10 @@ import {
   createHistory,
   createMemorySource,
 } from '@reach/router';
+import LocaleProvider from '@newrelic/gatsby-theme-newrelic/src/components/LocaleProvider';
+import themeTranslations from '@newrelic/gatsby-theme-newrelic/src/i18n/translations/en.json';
+import i18n from 'i18next';
+import { I18nextProvider } from 'react-i18next';
 
 import HomePage from '../index';
 import HomePageHighlights from '../../components/HomePageHighlights';
@@ -16,10 +20,39 @@ const source = createMemorySource('/');
 const history = createHistory(source);
 const pageContext = { fileRelativePath: 'src/pages/index.js' };
 
+const THEME_NAMESPACE = 'gatsby-theme-newrelic';
+
+const initI18n = () => {
+  i18n.init({
+    lng: 'en',
+    resources: {
+      en: {
+        [THEME_NAMESPACE]: themeTranslations,
+      },
+    },
+    defaultNS: 'translation',
+    initImmediate: false,
+    fallbackLng: 'en',
+    ns: [THEME_NAMESPACE, 'translation'],
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+};
+
 beforeEach(() => {
   useStaticQuery.mockImplementation(() => ({
     allMdx: {
       nodes: [],
+    },
+    allLocale: {
+      nodes: [{ name: 'English', isDefault: true }],
+    },
+    newRelicThemeConfig: {
+      forceTrailingSlashes: false,
     },
     site: {
       siteMetadata: {
@@ -37,10 +70,16 @@ beforeEach(() => {
 
 describe('HomePage', () => {
   it('Renders correctly', () => {
+    initI18n();
+
     const tree = TestRenderer.create(
-      <LocationProvider history={history}>
-        <HomePage pageContext={pageContext} />
-      </LocationProvider>
+      <I18nextProvider i18n={i18n}>
+        <LocaleProvider i18n={i18n}>
+          <LocationProvider history={history}>
+            <HomePage pageContext={pageContext} />
+          </LocationProvider>
+        </LocaleProvider>
+      </I18nextProvider>
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
@@ -48,10 +87,16 @@ describe('HomePage', () => {
 
 describe('HomePage Hero Header', () => {
   it('Displays the correct title', () => {
+    initI18n();
+
     const { getByTestId } = render(
-      <LocationProvider history={history}>
-        <HomePage pageContext={pageContext} />
-      </LocationProvider>
+      <I18nextProvider i18n={i18n}>
+        <LocaleProvider i18n={i18n}>
+          <LocationProvider history={history}>
+            <HomePage pageContext={pageContext} />
+          </LocationProvider>
+        </LocaleProvider>
+      </I18nextProvider>
     );
     const expectation = getByTestId('homepageHeroHeading');
     expect(expectation).toHaveTextContent(
@@ -62,10 +107,16 @@ describe('HomePage Hero Header', () => {
 
 describe('HomePage Projects We Support', () => {
   it('Renders correctly', () => {
+    initI18n();
+
     const testRenderer = TestRenderer.create(
-      <LocationProvider history={history}>
-        <HomePage pageContext={pageContext} />
-      </LocationProvider>
+      <I18nextProvider i18n={i18n}>
+        <LocaleProvider i18n={i18n}>
+          <LocationProvider history={history}>
+            <HomePage pageContext={pageContext} />
+          </LocationProvider>
+        </LocaleProvider>
+      </I18nextProvider>
     );
     const testInstance = testRenderer.root;
     const props = testInstance.findByType(HomePageHighlights).props;
@@ -76,10 +127,16 @@ describe('HomePage Projects We Support', () => {
 
 describe('HomePage Explore Projects', () => {
   it('renders correctly', () => {
+    initI18n();
+
     const testRenderer = TestRenderer.create(
-      <LocationProvider history={history}>
-        <HomePage pageContext={pageContext} />
-      </LocationProvider>
+      <I18nextProvider i18n={i18n}>
+        <LocaleProvider i18n={i18n}>
+          <LocationProvider history={history}>
+            <HomePage pageContext={pageContext} />
+          </LocationProvider>
+        </LocaleProvider>
+      </I18nextProvider>
     );
     const testInstance = testRenderer.root;
 
